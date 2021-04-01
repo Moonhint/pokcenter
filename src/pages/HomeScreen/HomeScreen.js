1f03737d8b21067437160e98120f9d3b4ad74aee
Antoni <antoni@Antonis-MacBook-Pro.local>
2021-04-01 10:38:52 +0700

    use lazy load image

--- src/pages/HomeScreen/HomeScreen.js
+++ src/pages/HomeScreen/HomeScreen.js
@@ -1,13 +1,14 @@
 // /** @jsxImportSource @emotion/react */
 import { Button } from 'antd';
 import { style } from './styles';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 import locale from './locale';
 import headerImg from './imgs/header.png';
 
 function HomeScreen() {
     return (
         <div css={style}>
-            <img className="header" src={headerImg} alt="" data-test="img-header"/>
+            <LazyLoadImage className="header" src={headerImg} alt="" data-test="img-header" />
             <div className="cta">
                 <a href="/my-pokemon" data-test="link-my-pokemon">
                     <Button type="ghost" size="large" shape="round">{locale.BTN_MY_POKEMON}</Button>
